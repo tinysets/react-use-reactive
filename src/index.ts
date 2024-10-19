cf87eb934bd4f86899e270b426c7bbb08d27f6a7
zhaolihang-com <625335512@qq.com>
2024-10-19 23:16:32 +0800

    update only 20 lines!

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { useReducer } from 'react'
 import { watch } from 'vue'
-
-
 export function useReactive(renderFun: () => JSX.Element | null) {
   let [_, forceUpdate] = useReducer((x) => x + 1, 0)
 
   let renderered = false;
-
   let reslut: JSX.Element | null = null
   watch(
     () => {
@@ -17,9 +14,7 @@ export function useReactive(renderFun: () => JSX.Element | null) {
     },
     () => {
       forceUpdate()
-      // console.log('forceUpdate')
     }, { deep: true, flush: "post", once: true }
   )
-
   return reslut
 }
